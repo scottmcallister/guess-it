b5e21d1079f8dca855abf6c0ac11898f79087878
Scott McAllister <scottm@nextopia.com>
2016-04-02 12:48:44 -0400

    Start new activity when main menu buttons are clicked

--- app/src/main/java/com/example/scott/guessit/MainActivity.java
+++ app/src/main/java/com/example/scott/guessit/MainActivity.java
@@ -1,17 +1,86 @@
 package com.example.scott.guessit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity /*implements SurfaceHolder.Callback*/{
+
+    /*protected static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 0;
+    private SurfaceView SurView;
+    private SurfaceHolder camHolder;
+    private boolean previewRunning;
+    final Context context = this;
+    public static Camera camera = null;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button galleryBtn = (Button) findViewById(R.id.gallery_btn);
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent goToGallery = new Intent(MainActivity.this, GalleryActivity.class);
+                MainActivity.this.startActivity(goToGallery);
+            }
+        });
+
+        final Button takePhotoBtn = (Button) findViewById(R.id.take_photo_btn);
+        takePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent goToGallery = new Intent(MainActivity.this, TakePhotoActivity.class);
+                MainActivity.this.startActivity(goToGallery);
+            }
+        });
+
+        /*SurView = (SurfaceView)findViewById(R.id.sview);
+        camHolder = SurView.getHolder();
+        camHolder.addCallback(this);
+        camHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);*/
     }
+
+    /*@Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+        if(previewRunning){
+            camera.stopPreview();
+        }
+        Camera.Parameters camParams = camera.getParameters();
+        Camera.Size size = camParams.getSupportedPreviewSizes().get(0);
+        camParams.setPreviewSize(size.width, size.height);
+        camera.setParameters(camParams);
+        try{
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+            previewRunning=true;
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try{
+            camera=Camera.open();
+        }catch(Exception e){
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera=null;
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
