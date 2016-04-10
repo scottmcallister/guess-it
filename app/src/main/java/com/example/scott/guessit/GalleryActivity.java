95d2e72d45eafde30450b7d6be9f7824844e8172
Scott McAllister <scottm@nextopia.com>
2016-04-10 18:21:36 -0400

    Change logic for reading photos from device. TO DO: fix permissions logic to ensure app has sufficient permissions to read photos.

--- app/src/main/java/com/example/scott/guessit/GalleryActivity.java
+++ app/src/main/java/com/example/scott/guessit/GalleryActivity.java
@@ -1,71 +1,103 @@
 package com.example.scott.guessit;
 
-import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
-
-import java.util.ArrayList;
+import android.widget.ImageView;
 
 public class GalleryActivity extends AppCompatActivity {
 
-    private static final String TAG = "GalleryActivity";
+    private Cursor imagecursor, actualimagecursor;
+    private int image_column_index, actual_image_column_index;
+    GridView imagegrid;
+    private int count;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-
-        GridView gridView = (GridView) findViewById(R.id.photo_grid);
-        Log.v(TAG, "Image count" + getImagesPath(this).size());
-        gridView.setAdapter(new ImageAdapter(this, getImagesPath(this)));
+        init_phone_image_grid();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gallery, menu);
-        return true;
+    private void init_phone_image_grid() {
+        String[] img = {MediaStore.Images.Thumbnails._ID};
+        imagecursor = managedQuery(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, img, MediaStore.Images.Thumbnails.DATA + "='/sdcard/DCIM/Camera/*.*'", null, MediaStore.Images.Thumbnails.IMAGE_ID + "");
+
+        image_column_index = imagecursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+        count = imagecursor.getCount();
+        imagegrid = (GridView) findViewById(R.id.photo_grid);
+        imagegrid.setAdapter(new ImageAdapter(getApplicationContext()));
+        imagegrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                System.gc();
+                String[] proj = {MediaStore.Images.Media.DATA};
+                actualimagecursor = managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, proj, MediaStore.Images.Media.DATA + "='/sdcard/DCIM/Camera/*.*'", null, null);
+                actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                actualimagecursor.moveToPosition(position);
+                String i = actualimagecursor.getString(actual_image_column_index);
+                System.gc();
+
+                Intent objI = getIntent();
+                objI.putExtra("clickedFile", i);
+                setResult(100, objI);
+                finish();
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public class ImageAdapter extends BaseAdapter
+    {
+        private Context mContext;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public ImageAdapter(Context c)
+        {
+            mContext = c;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    public static ArrayList<String> getImagesPath(Activity activity) {
-        Uri uri;
-        ArrayList<String> listOfAllImages = new ArrayList<String>();
-        Cursor cursor;
-        int column_index_data;
-        String PathOfImage = null;
-        uri = android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI;
-
-        String[] projection = {MediaStore.MediaColumns.DATA};
-
-        cursor = activity.getContentResolver().query(uri, projection, null, null, null);
-
-        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        while (cursor.moveToNext()) {
-            PathOfImage = cursor.getString(column_index_data);
-            listOfAllImages.add(PathOfImage);
+        public int getCount()
+        {
+            return count;
         }
-        return listOfAllImages;
+
+        public Object getItem(int position)
+        {
+            return position;
+        }
+
+        public long getItemId(int position)
+        {
+            return position;
+        }
+
+        public View getView(int position,View convertView,ViewGroup parent)
+        {
+            System.gc();
+            ImageView i = new ImageView(mContext.getApplicationContext());
+            if (convertView == null)
+            {
+                imagecursor.moveToPosition(position);
+                int id = imagecursor.getInt(image_column_index);
+
+                i.setImageURI(Uri.withAppendedPath( MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, "" + id));
+
+                i.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                i.setLayoutParams(new GridView.LayoutParams(92, 92));
+            }
+            else{
+                i = (ImageView) convertView;
+            }
+            return i;
+        }
     }
 }
+
